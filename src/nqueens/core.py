22ff0a94d1e0fdfdbc54f861320d3d16565b86a2
Ravindra <kshirsagarr@dnb.com>
2020-06-25 12:01:30 +0530

    Release v1.0

--- src/nqueens/core.py
+++ src/nqueens/core.py
@@ -4,16 +4,16 @@ from pprint import pprint as pp
 
 
 class NQueens(object):
-    def __init__(self, board_length=4, population_count=10, mutation_probability=0.025):
+    def __init__(self, board_length=4, population_count=10, mutation_probability=0.025, verbose=True):
         self.board_length = board_length
         self.population_count = population_count
-        self.initial_population = None
         self.population = None
         self.solution = None
         self.scores = None
         self.target_score = self.get_target_score()
         self.progress = []
         self.mutation_probability = mutation_probability
+        self.verbose = verbose
 
     def get_initial_population(self):
         first_list = np.array(range(0, self.board_length))
@@ -120,34 +120,45 @@ class NQueens(object):
             population[start_position][random_swap_index2] = val0
 
     def __call__(self):
-        print("Target Score is: {}".format(self.target_score))
+        if self.verbose:
+            print("Target Score is: {}".format(self.target_score))
 
+        # Set execution limits. If target solution not found in max generations then start from scratch again
+        # Do this till max attempts exceed and then give up with best solution.
         max_generations = 100
         max_attempts = 20
         best_solution = None
 
         for attempt in range(1, max_attempts):
-            print("\nStarting attempt #{}".format(attempt))
-
+            # Start from randomness
             self.population = self.get_initial_population()
-            self.initial_population = np.copy(self.population)
 
-            print("Initial Population:")
-            pp(self.population)
+            if self.verbose:
+                print("\nStarting attempt #{}".format(attempt))
+                print("Initial Population:")
+                pp(self.population)
 
             for generation_index in range(0, max_generations):
                 self.scores = self.evaluate_population()
                 self.progress.append(sum(self.scores))
-                print("Generation {} score: {}, Scores: {}".format(generation_index, sum(self.scores), self.scores))
+                if self.verbose:
+                    print("Generation {} score: {}, Scores: {}".format(generation_index, sum(self.scores), self.scores))
 
-                # sort population
+                # sort population based on scores
                 sorted_population = sorted(zip(self.scores, self.population), key=lambda x: x[0], reverse=True)
 
                 # check for target reached
                 if sorted_population[0][0] >= self.target_score:
-                    break
+                    target_solution = sorted_population[0][1]
+                    if self.verbose:
+                        print("\nFound Final Solution:")
+                        self.print_board(target_solution)
+                        print("\nFinal Solution: {}".format(target_solution))
+                    return target_solution
 
-                best_solution = sorted_population[0]
+                # update best solution so far
+                if not best_solution or best_solution[0] < sorted_population[0][0]:
+                    best_solution = sorted_population[0]
 
                 # produce children
                 new_population = []
@@ -161,6 +172,7 @@ class NQueens(object):
                 # mutation
                 self.mutate_population(population=new_population, mutation_probability=self.mutation_probability)
 
+                # Get scores of new population
                 new_scores = self.evaluate_population(population=new_population)
                 min_new_pop_score = min(new_scores)
 
@@ -168,32 +180,29 @@ class NQueens(object):
                 for score, individual in sorted_population:
                     if score > min_new_pop_score:
                         if (new_population == individual).all(1).any():
-                            # already present
+                            # This individual is already present
                             pass
                         else:
                             new_population.append(individual)
                             new_scores.append(score)
 
-                # select top scores from new population for gen next
+                # select top scores from new population for gen next keeping same population count
                 sorted_new_population = sorted(zip(new_scores, new_population), key=lambda x: x[0], reverse=True)
                 next_gen_population = []
                 for i in range(0, self.population_count):
                     next_gen_population.append(sorted_new_population[i][1])
+
+                # now we have got our next generation which is better than previous generation
                 self.population = next_gen_population
 
-            for (score, solution) in zip(self.scores, self.population):
-                if score == self.target_score:
-                    print("\nFound Final Solution:")
-                    self.print_board(solution)
-                    print("\nFinal Solution: {}".format(solution))
-                    return solution
-
-        print("\nFound best (but not target) Solution:")
-        self.print_board(best_solution[1])
-        print("\nBest Solution: {}".format(best_solution[1]))
+        # Could not reach final solution, but lets see what is the best solution we hav got
+        if self.verbose:
+            print("\nFound best (but not target) Solution:")
+            self.print_board(best_solution[1])
+            print("\nBest Solution: {}".format(best_solution[1]))
         return None
 
 
 if __name__ == "__main__":
-    n_queens = NQueens(8, 20, 0.15)
+    n_queens = NQueens(8, 40, 0.05)
     solution = n_queens()
--- src/tests/nqueens_test.py
+++ src/tests/nqueens_test.py
@@ -9,14 +9,14 @@ class TestCore(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_get_initial_population(self):
+    def test01_get_initial_population(self):
         obj = NQueens(4, 10)
         data = obj.get_initial_population()
         self.assertIsNotNone(data)
         self.assertEqual(len(data), 10, "Expecting 10 rows")
         self.assertEqual(len(data[0]), 4, "Expecting solution length 4")
 
-    def test_evaluate(self):
+    def test02_evaluate(self):
         obj = NQueens(4, 6)
         data = [2,0,1,3]
         score = obj.evaluate(data)
@@ -28,7 +28,7 @@ class TestCore(unittest.TestCase):
         score = obj.evaluate(data)
         self.assertEqual(score, 5)
 
-    def test_get_target_score(self):
+    def test03_get_target_score(self):
         obj = NQueens(4, 6)
         self.assertEqual(6, obj.get_target_score())
         obj = NQueens(5, 6)
@@ -36,16 +36,23 @@ class TestCore(unittest.TestCase):
         obj = NQueens(8, 6)
         self.assertEqual(28, obj.get_target_score())
 
-    def test_print_board(self):
-        obj = NQueens(8, 10)
-        solution = [7, 1, 6, 5, 2, 4, 0, 3]
-        score = obj.evaluate(solution)
-        obj.print_board(solution)
-        self.assertNotEqual(score, obj.target_score)
-
-    def test_final_solution(self):
+    def test04_final_solution(self):
         obj = NQueens(8, 10)
         solution = [2, 5, 3, 0, 7, 4, 6, 1]
         score = obj.evaluate(solution)
         self.assertEqual(obj.target_score, 28)
+        self.assertEqual(score, obj.target_score)
+
+    def test05_generate_8x8_solution(self):
+        obj = NQueens(8, 40, 0.05, False)
+        solution = obj()
+        self.assertIsNotNone(solution)
+        score = obj.evaluate(solution)
+        self.assertEqual(score, obj.target_score)
+
+    def test06_generate_10x10_solution(self):
+        obj = NQueens(10, 100, 0.025, False)
+        solution = obj()
+        self.assertIsNotNone(solution)
+        score = obj.evaluate(solution)
         self.assertEqual(score, obj.target_score)
